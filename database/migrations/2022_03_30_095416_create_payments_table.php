db39225fb191a722eb0a55f8f1699850e2f9d35e
hormeku mawuli <hormekumawuli93@gmail.com>
2022-03-30 09:55:30 +0000

    payment table

---
+++ database/migrations/2022_03_30_095416_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer("rental_id");
+            $table->float("payment_amount");
+            $table->float("add_charges");
+            $table->date("payment_date");
+            $table->integer("admin_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
