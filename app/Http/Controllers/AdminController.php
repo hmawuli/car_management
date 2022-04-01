e754d9d8a93a9e7f681c2951b4d54d6b2acdaf96
hormeku mawuli <hormekumawuli93@gmail.com>
2022-04-01 07:42:14 +0000

    update_car function in the admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class AdminController extends Controller
+
 {
      public function add_car(Request $request){
          $car_name = $request->car_name;
@@ -19,6 +20,7 @@ class AdminController extends Controller
          $owner_id = $request->owner_id;
          $status = $request->status;
             return Inertia::render('admin/add_car');
+
         //  $data =[
         //      'car_name' => '',
         //      'description' => '',
@@ -32,4 +34,18 @@ class AdminController extends Controller
         //  ];
         //  Car::create($data);
     }
+        public function update_car(Request $request){
+            $car_name = $request-> car_name;
+            $description = $request->description;
+         $model_year = $request->model_year;
+         $car_brand = $request->car_brand;
+         $color = $request->color;
+         $capacity = $request->capacity;
+         $plate_number = $request->plate_number;
+         $rate = $request->rate;
+         $owner_id = $request->owner_id;
+         $status = $request->status;
+         return Inertia::render('update_car/store');
+        }
+
 }
